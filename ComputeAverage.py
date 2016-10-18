d3d95b031ec42144973f500559438607c8f36599
longlongman <1050466028@qq.com>
2016-10-18 17:24:29 +0800

    new file

---
+++ ComputeAverage.py
@@ -0,0 +1,7 @@
+number1 = eval(input())
+number2 = eval(input())
+number3 = eval(input())
+
+average = (number1 + number2 + number3)/3
+
+print("the average of ",number1,number2,number3,"is",average)
